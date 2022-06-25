08a4d7603e7b14d9fe270638c48ff0ab381aa5e2
Cozmo <90281965+Cozmo007@users.noreply.github.com>
2022-06-24 21:35:59 -0300

    Update portscanner.php

--- portscanner.php
+++ portscanner.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
 $ip = readline("IP: ");
-$portas = array(53, 80, 8000, 8080, 4444, 5000, 9080, 7070,);
+$portas = array(53, 80, 8000, 8080, 4444, 5000, 9080, 7070);
 foreach($portas as $porta){
 	$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 	$re = socket_connect($sock, $ip, $porta);
